64f96a727ac2a1133562a84685cba3a029bdf9de
MTA2002 <104497136+MTA2002@users.noreply.github.com>
2023-01-26 23:06:37 +0300

    Update Stack implementation using array.cpp

--- Stack implementation using array.cpp
+++ Stack implementation using array.cpp
@@ -1,126 +1,76 @@
-#include<iostream>
+//Array based stack implementation
+#include <iostream>
 using namespace std;
 
-int MAX_STACK=10;
-int STACK[10];
+const int MAX_SIZE=5;
+
 int top=-1;
+int stack_array[MAX_SIZE];
 
 void main_menu();
-void push(int number);
-int pop();
-void peek();
-void display();
-void make_empty();
-bool is_full();
 bool is_emptyy();
-int getSize();
-
+bool is_full();
+void display();
+int peek();
+void push(int number);
+void pop();
+void make_empty();
+int get_size();
 
 int main(){
     main_menu();
-
+    
     return 0;
 }
 void main_menu(){
     int choice;
+    int number;
     do {
-        system("cls");
-        cout<<"WELCOME TO STACK IMPLEMENTATION USING ARRAYS!!"<<endl;
-        cout<<"1. PUSH ELEMENT TO STACK"<<endl;
-        cout<<"2. DISPLAY ALL ELEMENT OF STACK"<<endl;
-        cout<<"3. POP ELEMENT FROM STACK"<<endl;
-        cout<<"4. PEEK TO AN ELEMENT FROM THE STACK"<<endl;
-        cout<<"5. MAKE THE STACK EMPTY"<<endl;
-        cout<<"6. FIND THE SIZE OF THE STACK"<<endl;
-        cout<<"7. EXIT"<<endl;
-        cout<<"PLEASE INPUT YOUR CHOICE: ";
+        cout<<"1. Push an element to the stack"<<endl;
+        cout<<"2. Pop an element to the stack"<<endl;
+        cout<<"3. Display all element from the stack"<<endl;
+        cout<<"4. Peek the top element of the stack"<<endl;
+        cout<<"5. Make the stack empty"<<endl;
+        cout<<"6. Get the size of the stack"<<endl;
+        cout<<"7. Exit"<<endl;
+        cout<<"Input you choice: ";
         cin>>choice;
         switch (choice) {
-                system("cls");
             case 1:
-                int number;
-                cout<<"ENTER THE NUMBER YOU WANT TO PUSH TO THE STACK: ";
+                cout<<"Enter a number: ";
                 cin>>number;
                 push(number);
-                system("pause");
                 break;
             case 2:
-                display();
-                system("pause");
-                break;
-            case 3:
                 pop();
                 break;
+            case 3:
+                display();
+                break;
             case 4:
-                peek();
-                system("pause");
+                if (is_emptyy()) {
+                    cout<<"No element to peek!!"<<endl;
+                } else {
+                    cout<<"The top element from the stack is "<<peek()<<endl;;
+                }
                 break;
             case 5:
                 make_empty();
-                system("pause");
                 break;
             case 6:
-                cout<<"THE SIZE OF THE STACK IS: "<<getSize();
-                system("pause");
+                if (is_emptyy()) {
+                    cout<<"The stack is empty!!"<<endl;
+                } else {
+                    cout<<"The size of the stack is "<<get_size()<<endl;;
+                }
                 break;
-
             default:
+                cout<<"Please enter appropriate value!!"<<endl;
+                main_menu();
                 break;
         }
 
     } while (choice!=7);
-}
-void push(int number){
-    if (is_full()!=true) {
-        top++;
-        STACK[top]=number;
-        cout<<"ELEMENT SUCCESSFULLY PUSHED!!"<<endl;
-    } else {
-        cout<<"STACK OVERFLOW!!"<<endl;
-    }
-}
-int pop(){
-    if (is_emptyy()!=true) {
-        int element_at_top;
-        element_at_top=STACK[top];
-        STACK[top]=NULL;
-        top--;
-
-        return element_at_top;
-
-    } else {
-        cout<<"STACK UNDERFLOW!!"<<endl;
-        return 0;
-    }
-
-}
-void peek(){
-    if (is_emptyy()!=true) {
-        cout<<"ELEMENT AT TOP IS "<<STACK[top]<<endl;
-    } else {
-        cout<<"NO ELEMENT TO PEEK"<<endl;
-    }
-}
-void display(){
-    if (is_emptyy()!=true) {
-        while (is_emptyy()!=true) {
-            cout<<pop()<<"  ";
-
-        }
-    } else {
-        cout<<"STACK IS EMPTY"<<endl;
-    }
-}
-void make_empty(){
-    top=-1;
-    cout<<"STACK IS NOW EMPTY!!"<<endl;
-}
-bool is_full(){
-    if (top>MAX_STACK) {
-        return true;
-    } else{
-        return false;
-    }
 }
 bool is_emptyy(){
     if (top==-1) {
@@ -129,11 +79,58 @@ bool is_emptyy(){
         return false;
     }
 }
-int getSize(){
-    if (top>-1) {
-        return top+1;
+bool is_full(){
+    if (top+1==MAX_SIZE) {
+        return true;
     } else {
-        return 0;
+        return false;
     }
-
+}
+void display(){
+    if (is_emptyy()) {
+        cout<<"The stack is empty,no element to display!!"<<endl;
+    } else {
+        cout<<"The elements in the stack are: ";
+        for (int i=top; i>=0; i--) {
+            cout<<stack_array[i]<<" ";
+        }
+        cout<<endl;
+    }
+}
+int peek(){
+    return stack_array[top];
+}
+void push(int number){
+    if (is_full()) {
+        cout<<"Stack Overflow!!"<<endl;
+    } else {
+        top++;
+        stack_array[top]=number;
+        cout<<number<<" succesfully pushed to the stack!!"<<endl<<endl;
+    }
+}
+void pop(){
+    if (is_emptyy()) {
+        cout<<"Stack Underflow!!"<<endl;
+    } else {
+        int number;
+        number=stack_array[top];
+        top--;
+        cout<<number<<" succesfully popped from the stack!!"<<endl<<endl;
+    }
+}
+void make_empty(){
+    if (is_emptyy()) {
+        cout<<"The stack is currently empty!!"<<endl;
+    } else {
+        top=-1;
+        cout<<"The stack have been reinitialized to empty!!"<<endl;
+    }
+}
+int get_size(){
+    if (is_emptyy()) {
+        return -1;
+    } else {
+        return top+1;
+    }
 }
